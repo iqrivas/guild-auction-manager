5fc1c442db2fe5501f95fdba1a1376a843a8e47e
Denisse Rivas <iqrivas@gmail.com>
2022-07-16 00:42:07 -0500

    Add annotate gem

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: auctions
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  date             :date
+#  status           :string
+#  amount_collected :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 class Auction < ApplicationRecord
 end
--- test/models/auction_test.rb
+++ test/models/auction_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: auctions
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  date             :date
+#  status           :string
+#  amount_collected :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require "test_helper"
 
 class AuctionTest < ActiveSupport::TestCase
