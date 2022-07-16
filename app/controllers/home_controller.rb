e0c426dfa8b2e0465968303baccf61ad05c53b47
Denisse Rivas <iqrivas@gmail.com>
2022-07-15 21:17:09 -0500

    Use ERB and instance variables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
     def index
+        @nombre = "Denisse Rivas" #Variable de instancia
+        @skills = ["HTML", "CSS", "Ruby", "Rails", "JavaScript", "React"]
     end
 end
